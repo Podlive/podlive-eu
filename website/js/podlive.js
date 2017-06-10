ad866840abdd381843f990fe2b2036ecfc1073e5
Frank Gregor <phranck@cocoanaut.com>
2017-06-11 00:43:44 +0200

    🤔 updates JS

--- website/js/podlive.js
+++ website/js/podlive.js
@@ -1,3 +1,3 @@
 function comingSoon() {
-    swal("Wir testen...", "Momentan befinden wir uns in der Beta-Testphase. Podlive wird bald im App Store verfügbar sein.\n\nBitte hab noch ein bisschen Geduld. ☺️");
+    swal("Wir testen...", "Momentan befinden wir uns in der Beta-Testphase.\nPodlive wird bald im App Store verfügbar sein.\n\nBitte hab noch ein bisschen Geduld. ☺️");
 }
