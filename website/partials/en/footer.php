f5e72806ef969e43b43848e0e360656a544fd3ee
Frank Gregor <phranck@cocoanaut.com>
2017-06-12 12:42:35 +0200

    🤔 removes html comments

--- website/partials/en/footer.php
+++ website/partials/en/footer.php
@@ -1,5 +1,4 @@
 
-        <!-- Footer -->
         <footer class="bottom">
             <span class="maker">iOS App by <a href="http://stefantrauth.de">Stefan Trauth</a>, macOS App by <a href="https://cocoanaut.com">Frank Gregor</a></span>
             <span class="share"><a target="_blank" class="twitter" href="https://twitter.com/podliveapp">@PodliveApp</a></span>
