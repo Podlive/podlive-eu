79708e4ede27dfc326cf6904a01d3352eae1aa03
Frank Gregor <phranck@cocoanaut.com>
2017-07-13 10:12:33 +0200

    🐛 fixes menu and language selector creation

--- website/site/snippets/menu.php
+++ website/site/snippets/menu.php
@@ -1,13 +1,13 @@
 	<nav class="top">
-		<?php foreach($pages->visible() as $page): ?>
-		<?php if($page->isMenuItem()->bool()): ?>
-		<div class="menuitem"><a href="<?= $page->url() ?>"><?= $page->menuTitle()->text() ?></a></div>
+		<?php foreach($pages->visible() as $thePage): ?>
+		<?php if($thePage->isMenuItem()->bool()): ?>
+		<div class="menuitem"><a href="<?= $thePage->url() ?>"><?= $thePage->menuTitle()->text() ?></a></div>
 		<?php endif ?>
 		<?php endforeach ?>
 		<ul>
 			<?php foreach($site->languages() as $language): ?>
 			<li<?php e($site->language() == $language, ' class="active"') ?>>
-			<a href="<?= $language->url() ?>" class="language <?= $language->code() ?>"></a>
+			<a href="<?= $page->url($language->code()) ?>" class="language <?= $language->code() ?>"></a>
 			</li>
 			<?php endforeach ?>
 		</ul>
