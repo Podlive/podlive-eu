97ba2c801eac6fda02ab8460b5abbd54364a79d0
Stefan Trauth <mail@stefantrauth.de>
2017-11-12 11:17:48 +0100

    add contact snippet

---
+++ website/site/snippets/contact.php
@@ -0,0 +1,11 @@
+
+        <section id="contact" class="content gradient">
+            <div class="container">
+                <div class="row clearfix">
+                    <div class="column full">
+                        <h2><?= $section->title()->text() ?></h2>
+                        <?= $section->text()->kirbytext() ?>
+                    </div>
+                </div>
+            </div>
+        </section>
