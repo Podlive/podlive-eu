d22a1b7f4c11589e677b8f889127f79a9ee40067
Stefan Trauth <mail@stefantrauth.de>
2018-02-28 12:00:52 +0100

    test showing channel data

--- website/site/templates/channel.php
+++ website/site/templates/channel.php
@@ -1,5 +1,28 @@
 
-<?php snippet('header', array('context' => 'podcast', 'channel' => $channel)) ?>
+<?php snippet('header', array('context' => 'channel', 'channel' => $channel)) ?>
+
+<?php
+$name           = trim($channel->get("name"));
+$followerCount  = $channel->get("followerCount");
+$listenerCount  = $channel->get("listenerCount");
+$coverArtUrl    = $channel->get("coverartThumbnail200")->getURL();
+$websiteUrl     = $channel->get("websiteUrl");
+$channelState   = $channel->get("state");
+$isOnline       = ($channelState != "offline");
+
+$followerLabel = "";
+$listenerLabel = "";
+
+if($listenerCount == 1) {
+    $followerLabel = $page->followerCountTextSingular()->text();
+    $listenerLabel = $page->listenerCountTextSingular()->text();
+}
+else {
+    $followerLabel = $page->followerCountTextPlural()->text();
+    $listenerLabel = $page->listenerCountTextPlural()->text();
+}
+
+?>
 
 <section class="article">
 	<div class="container blog">
@@ -8,10 +31,7 @@
                 <h1 class="article-title"><?= $page->title()->html() ?></h1>
             </header>
 
-            <div class="text">
-				<?php snippet('coverimage', $page) ?>
-                <?= $page->text()->kirbytext() ?>
-            </div>
+            <img class="channel-logo" src="<?= $coverArtUrl ?>">
         </article>
 
 	</div>
