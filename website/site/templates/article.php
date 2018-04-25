0380924c593686bf7641ad6071dbbb9c63a6f383
Frank Gregor <phranck@cocoanaut.com>
2018-04-26 01:26:56 +0200

    adds disqus to article

--- website/site/templates/article.php
+++ website/site/templates/article.php
@@ -2,22 +2,22 @@
 
 	<section class="article">
 		<div class="container blog">
-            <article>
-                <header class="article-header">
-                    <h1 class="article-title"><?= $page->title()->html() ?></h1>
-                    <?php snippet('article-date-author', array('article' => $page)) ?>
-                </header>
+      <article>
+          <header class="article-header">
+              <h1 class="article-title"><?= $page->title()->html() ?></h1>
+              <?php snippet('article-date-author', array('article' => $page)) ?>
+          </header>
 
-                <div class="text">
-					<?php snippet('coverimage', $page) ?>
-                    <?= $page->text()->kirbytext() ?>
-                </div>
-            </article>
+          <div class="text">
+						<?php snippet('coverimage', $page) ?>
+						<?= $page->text()->kirbytext() ?>
+          </div>
+      </article>
 
-	    <?php snippet('prevnext', ['flip' => true, 'page' => $page]) ?>
+			<?php snippet('prevnext', ['flip' => true, 'page' => $page]) ?>
+			<?php snippet('disqus', array('disqus_shortname' => 'podlive')) ?>
 
 		</div>
 	</section>
 
 <?php snippet('footer') ?>
-
