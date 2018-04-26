b9c57e6c446c7fc875850756d0e9054c7c876b8e
Frank Gregor <phranck@cocoanaut.com>
2018-04-26 02:06:00 +0200

    updates navigation snippet

--- website/site/snippets/prevnext.php
+++ website/site/snippets/prevnext.php
@@ -17,26 +17,29 @@ https://getkirby.com/docs/templates/snippets
 $directionPrev = @$flip ? 'right' : 'left';
 $directionNext = @$flip ? 'left'  : 'right';
 
+$titlePrev = @$flip ? 'vorheriger →' : '← nächster';
+$titleNext = @$flip ? '← nächster' : 'vorheriger →';
+
 if($page->hasNextVisible() || $page->hasPrevVisible()): ?>
   <nav class="pagination <?= !@$flip ?: ' flip' ?> wrap cf">
 
     <?php if($page->hasPrevVisible()): ?>
       <a class="pagination-item <?= $directionPrev ?>" href="<?= $page->prevVisible()->url() ?>" rel="prev" title="<?= $page->prevVisible()->title()->html() ?>">
-        <?= (new Asset("assets/images/arrow-{$directionPrev}.svg"))->content() ?>
+        <?= $titlePrev ?>
       </a>
     <?php else: ?>
       <span class="pagination-item <?= $directionPrev ?> is-inactive">
-        <?= (new Asset("assets/images/arrow-{$directionPrev}.svg"))->content() ?>
+        <?= $titlePrev) ?>
       </span>
     <?php endif ?>
 
     <?php if($page->hasNextVisible()): ?>
       <a class="pagination-item <?= $directionNext ?>" href="<?= $page->nextVisible()->url() ?>" rel="next" title="<?= $page->nextVisible()->title()->html() ?>">
-        <?= (new Asset("assets/images/arrow-{$directionNext}.svg"))->content() ?>
+        <?= $titleNext ?>
       </a>
     <?php else: ?>
       <span class="pagination-item <?= $directionNext ?> is-inactive">
-        <?= (new Asset("assets/images/arrow-{$directionNext}.svg"))->content() ?>
+        <?= $titleNext ?>
       </span>
     <?php endif ?>
 
