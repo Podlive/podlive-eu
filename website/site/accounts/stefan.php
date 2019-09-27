f89e285c6414066c0bc78d391b506f397a287257
Frank Gregor <phranck@cocoanaut.com>
2019-09-27 23:00:08 +0200

    [Cron Backup]

--- website/site/accounts/stefan.php
+++ website/site/accounts/stefan.php
@@ -9,11 +9,11 @@ password: >
 language: en
 role: admin
 history:
+  - blog/zu-gast-im-audiodump-podcast
   - blog/podlive-jetzt-optimiert-fuer-ios-13
   - blog/podlive-testbericht-von-podcatcher-ch
   - terms-of-service
   - blog/was-ist-mit-dem-chat-passiert
-  - blog/podlive-jetzt-endlich-mit-nachtmodus
 bio: ""
 twitter: funkenstrahlen
 github: funkenstrahlen
