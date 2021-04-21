e1516fd773c9ced95ce1ff383a4ea808ca2ae6a8
Stefan Trauth <mail@stefantrauth.de>
2021-04-21 17:19:24 +0200

    add account for sven

---
+++ podlive.io/site/accounts/sven.php
@@ -0,0 +1,20 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: sven
+firstname: Sven
+lastname: Hennessen
+email: sven@hennessen.net
+password: >
+  $2y$10$iplQJhExrKmSzzWWDvvc/euu1KAf1OKBxAqsOVK8/RNsM1Nh2dNDW
+language: de
+role: admin
+history:
+  - blog/podlive-fuer-android-ist-da
+bio: ""
+twitter: @svenhennessen
+github: ""
+facebook: ""
+linkedin: ""
+xing: ""
+showauthor: "1"
+linkauthor: twitter
