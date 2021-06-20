ade47cffb9629d318c73145fa7188aba78ce1878
Stefan Trauth <mail@stefantrauth.de>
2021-06-20 14:18:26 +0200

    add google playstore button

--- podlive.io/site/snippets/appstore-buttons.php
+++ podlive.io/site/snippets/appstore-buttons.php
@@ -2,4 +2,5 @@
 				<div class="column center">
 					<a href="<?= $site->iosAppstore()->text() ?>" class="button">iOS App Store</a>
 					<a href="<?= $site->macAppstore()->text() ?>" class="button">Mac App Store</a>
+					<a href="<?= $site->googlePlaystore()->text() ?>" class="button">Google Play Store</a>
 				</div>
