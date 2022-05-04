477b0f4e8afe5ed1a6f7e0b717639b1782ddde72
Stefan Trauth <mail@stefantrauth.de>
2022-05-04 14:35:17 +0200

    add mastodon and twitter link to footer

--- podlive.io/site/snippets/footer.php
+++ podlive.io/site/snippets/footer.php
@@ -4,6 +4,8 @@
 		<p class="maker"><?= $site->copyright()->text() ?></p>
 		<p class="share"><a href="https://podlive.io/privacy-policy">Datenschutzerklärung</a></p>
 		<p class="share"><a href="https://podlive.io/imprint">Impressum</a></p>
+		<p class="share"><a rel="me" href="https://chaos.social/@Podlive">Mastodon</a></p>
+        <p class="share"><a href="https://twitter.com/podliveapp">Twitter</a></p>
 	</footer>
 
 	</div>
