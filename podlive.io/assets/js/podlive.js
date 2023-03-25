896524a40bd4f830cf76ad60743731fa01307f02
Stefan Trauth <mail@stefantrauth.de>
2023-03-25 18:35:47 +0100

    replace kirby with hugo template

--- podlive.io/assets/js/podlive.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-    $.fn.goTo = function() {
-        $('html, body').animate({
-            scrollTop: $(this).offset().top + 'px'
-        }, 'slow');
-        return this; // for chaining...
-    };
-})(jQuery);
--- podlive.io/site/snippets/appstore-buttons.php
+++
@@ -1,6 +0,0 @@
-
-				<div class="column center">
-					<a href="<?= $site->iosAppstore()->text() ?>" class="button">iOS App Store</a>
-					<a href="<?= $site->macAppstore()->text() ?>" class="button">Mac App Store</a>
-					<a href="<?= $site->googlePlaystore()->text() ?>" class="button">Google Play Store</a>
-				</div>
